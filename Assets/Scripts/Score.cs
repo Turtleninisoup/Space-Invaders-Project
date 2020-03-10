e930aa745a8d26e5ad2342248b676a33d5b01a3b
Turtleninisoup <60590189+Turtleninisoup@users.noreply.github.com>
2020-03-09 23:26:09 -0700

    Began GUI.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    //Variables
+    private int score = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Getter and setter
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void setScore(int s)
+    {
+        score = s;
+    }
+}
