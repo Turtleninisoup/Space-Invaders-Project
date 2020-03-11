803904aefc66fe5dccc64df70181ddf13a8e5d30
Turtleninisoup <60590189+Turtleninisoup@users.noreply.github.com>
2020-03-11 01:36:56 -0700

    Added enemy movement.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,12 @@ public class Enemy : MonoBehaviour
     //Variables
     Score score;
     public int worth;
+    float speed = 1;
+    bool moveRight = true;
+    double lbounds = -7.5;
+    double rbounds = 7.5;
+    Vector3 moveDown = new Vector3(0f, -0.5f, 0f);
+    public Transform transform;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +20,35 @@ public class Enemy : MonoBehaviour
         score = GameObject.Find("Manager").GetComponent<Score>();
     }
 
+    // Update is called once per frame
+    private void Update()
+    {
+        if ((transform.position.x >= rbounds) || (transform.position.x <= lbounds))
+        {
+            moveRight = !moveRight;
+            DownMovement();
+            print("Switching directions; now moving " + (moveRight ? "Right" : "Left"));
+        }
+        Movement(moveRight);
+    }
+
+    //Method for shooting enemy
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
         score.setScore(score.getScore() + worth);
         Debug.Log("Ouch!");
+    }
+
+    //Movement methods
+    void Movement(bool right)
+    {
+        float movementX = right ? speed : -speed;
+        transform.Translate(new Vector3(movementX * Time.deltaTime, 0, 0));
+    }
+
+    void DownMovement()
+    {
+        transform.Translate(moveDown);
     }
 }
